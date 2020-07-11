d7b265c2847313435c88ec79b3479055c33736a3
Yichen <msdaiyichen@gmail.com>
2020-07-10 23:10:25 -0700

    Updates

---
+++ precache-manifest.493046bd1c046f290d05126529825f42.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580a31f2ca2bc6ed4abeac70a9627fca",
+    "url": "/devloperPortfolio/index.html"
+  },
+  {
+    "revision": "f8dd7d40ade7bcb3c73b",
+    "url": "/devloperPortfolio/static/css/3.b499fcd2.chunk.css"
+  },
+  {
+    "revision": "9cc2f9ab85b29083a49c",
+    "url": "/devloperPortfolio/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "52a35819f0534325d701",
+    "url": "/devloperPortfolio/static/css/main.d5499654.chunk.css"
+  },
+  {
+    "revision": "445585ab3c6c7d0e5ad8",
+    "url": "/devloperPortfolio/static/js/2.4fb1546c.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/devloperPortfolio/static/js/2.4fb1546c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8dd7d40ade7bcb3c73b",
+    "url": "/devloperPortfolio/static/js/3.951448b1.chunk.js"
+  },
+  {
+    "revision": "9cc2f9ab85b29083a49c",
+    "url": "/devloperPortfolio/static/js/4.d2d58457.chunk.js"
+  },
+  {
+    "revision": "52a35819f0534325d701",
+    "url": "/devloperPortfolio/static/js/main.c850ccc7.chunk.js"
+  },
+  {
+    "revision": "c76668bf4905e1e8c246",
+    "url": "/devloperPortfolio/static/js/runtime-main.3f302008.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/devloperPortfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "bf10fe1ca3d75c279c5b6f55c295d4d5",
+    "url": "/devloperPortfolio/static/media/Longfor20Properties.bf10fe1c.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/devloperPortfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "0ed248947ba97085cf9805bd25f840c3",
+    "url": "/devloperPortfolio/static/media/bestNeighbor.0ed24894.png"
+  },
+  {
+    "revision": "e90abac4c2707c8c136015ee6879cc08",
+    "url": "/devloperPortfolio/static/media/contactMe.e90abac4.png"
+  },
+  {
+    "revision": "c7ddfa51a6a1bbd643791dcfd0d227a7",
+    "url": "/devloperPortfolio/static/media/dialoggBox.c7ddfa51.png"
+  },
+  {
+    "revision": "2e0a88c58e9709aa1dfdd98fcc89dcf9",
+    "url": "/devloperPortfolio/static/media/greeting.2e0a88c5.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/devloperPortfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "1646dae6f24d4225ebe051416822964c",
+    "url": "/devloperPortfolio/static/media/proficiency.1646dae6.png"
+  },
+  {
+    "revision": "6185e552b47cd9df63e077074d7c266f",
+    "url": "/devloperPortfolio/static/media/smartInvestor.6185e552.png"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/devloperPortfolio/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "0e739ab2c87299ef9dc1032251881b90",
+    "url": "/devloperPortfolio/static/media/tomtom.0e739ab2.png"
+  },
+  {
+    "revision": "50c1f0a04566ce305f20601233e06033",
+    "url": "/devloperPortfolio/static/media/whatIdo.50c1f0a0.png"
+  }
+]);
